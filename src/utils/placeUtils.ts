4695969136382e713332dc2354db142faa9ec520
roy845 <royatali94@gmail.com>
2025-07-22 19:23:04 +0300

    First Commit

--- src/utils/placeUtils.ts
+++ src/utils/placeUtils.ts
@@ -1,14 +1,25 @@
 import { Place } from "../types/places.types";
+
 export class PlaceUtils {
-  static validateNewPlace(place: Place, existingPlaces: Place[]) {
-    const exists = existingPlaces.some(
+  static validateNewPlace(place: Place, existingPlaces: Place[]): void {
+    const existsByNameAndAddress: boolean = existingPlaces.some(
       (p: Place) =>
         p.name.toLowerCase().trim() === place.name.toLowerCase().trim() &&
         p.address.toLowerCase().trim() === place.address.toLowerCase().trim()
     );
 
-    if (exists) {
+    if (existsByNameAndAddress) {
       throw new Error("Place with the same name and address already exists.");
+    }
+
+    const existsByCoordinates: boolean = existingPlaces.some(
+      (p: Place) => p.lat === place.lat && p.lng === place.lng
+    );
+
+    if (existsByCoordinates) {
+      throw new Error(
+        "Place with the same latitude and longitude already exists."
+      );
     }
   }
 }
