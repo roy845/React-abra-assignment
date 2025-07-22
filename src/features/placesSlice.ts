7b51765f87d58205674c5b643c9806365ec92ad1
roy845 <royatali94@gmail.com>
2025-07-22 19:12:17 +0300

    First Commit

--- src/features/placesSlice.ts
+++ src/features/placesSlice.ts
@@ -11,17 +11,7 @@ export const placesSlice = createSlice({
   initialState,
   reducers: {
     addPlace(state, action: PayloadAction<Place>) {
-      const { name, address } = action.payload;
-      const exists: boolean = state.places.some(
-        (place) =>
-          place.name.toLowerCase() === name.toLowerCase().trim() &&
-          place.address.toLowerCase() === address.toLowerCase().trim()
-      );
-      if (!exists) {
-        state.places.push(action.payload);
-      } else {
-        throw new Error("Place with the same name and address already exists.");
-      }
+      state.places.push(action.payload);
     },
     setSelectedPlace(state, action: PayloadAction<Place>) {
       state.selectedPlace = action.payload;
